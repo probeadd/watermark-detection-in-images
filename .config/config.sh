af2fc55ea69707d124fa06bf53571b259d9564e0
probeadd <prabowo.satrioadi@gmail.com>
2024-10-20 11:54:57 +0700

    Add .config to store config.sh

---
+++ .config/config.sh
@@ -0,0 +1,3 @@
+export LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=true
+export LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=/Users/probeadd/Projects/watermark-detection-in-images/data/images
+export MODEL_DIR=/Users/probeadd/Projects/watermark-detection-in-images/models/faster_rcnn_resnet50.pt
